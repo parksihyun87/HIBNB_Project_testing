b5990eb5a20d1c4c3ac2e90fca02e78a1c001afd
choi-seonah <dondo0412@naver.com>
2025-06-12 11:07:36 +0900

    verify confirm id

--- frontend/src/ReConfirmID.js
+++ frontend/src/ReConfirmID.js
@@ -6,19 +6,19 @@ export default function ReConfirmID(){
     const navigate=useNavigate();
 
     const [email, setEmail] = useState("");
-    const [authenNum, setAuthenNum] = useState("");
+    const [code, setCode] = useState("");
+    const [emailAuthenSent, setEmailAuthenSent] = useState(false);
 
     const successAuthen=async (e)=>{
         e.preventDefault();
         try {
-            const response = await apiClient.post("http://localhost:8080/re-confirm-id", {
-                email,
-                authenNum,
+            const response = await apiClient.get("http://localhost:8080/re-confirm-id", {
+                params:{email, code}
             });
 
             // 서버에서 받은 아이디 출력
-            if (response.data && response.data.userId) {
-                alert(`당신의 아이디는 "${response.data.userId}" 입니다.`);
+            if (response.data) {
+                alert(`당신의 아이디는 "${response.data}" 입니다.`);
                 navigate("/login");
             } else {
                 alert("인증번호가 일치하지 않거나 정보가 올바르지 않습니다.");
@@ -29,28 +29,44 @@ export default function ReConfirmID(){
         }
     }
 
+    const handleSendEmail=async (e)=>{
+        e.preventDefault();
+        try{
+            const response=await apiClient.get(`http://localhost:8080/email?email=${email}`);
+
+            if(response.data){
+                alert("인증번호가 이메일로 전송되었습니다.");
+                setEmailAuthenSent(true);
+
+            }
+        }catch(error){
+            console.error(error);
+            alert("오류가 발생했습니다. 다시 시도해주세요. ");
+        }
+    }
+
     return(
         <>
             <h2>아이디 찾기</h2>
             <form onSubmit={successAuthen}>
-
-                <label>이메일을 입력해주세요. 입력하신 이메일로 인증번호가 전송됩니다.</label>
+                <label>이메일을 입력해주세요. 입력하신 이메일로 인증번호가 전송됩니다.</label><br/>
                 <input
                     type="text"
                     name="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                /><br/>
+                />
+                <button onClick={handleSendEmail}>인증번호 전송</button><br/>
 
                 <label>인증번호를 입력해주세요.</label>
                 <input
                     type="password"
                     name="authennum"
-                    value={authenNum}
-                    onChange={(e) => setAuthenNum(e.target.value)}
-                /><br/>
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                />
 
-                <button type="submit">인증하기</button>
+                <button type="submit" disabled={!code}>인증하기</button>
             </form>
         </>
     );
